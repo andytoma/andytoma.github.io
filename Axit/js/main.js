02dd7e838395423e996fb2c917eb0cab61f03d51
andytoma <43856136+andytoma@users.noreply.github.com>
2020-06-18 21:55:59 +0300

    Add files via upload

---
+++ Axit/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('.header__burger').click(function(event){
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+
+
+$(function() {  
+    $('.btn-6')
+      .on('mouseenter', function(e) {
+              var parentOffset = $(this).offset(),
+              relX = e.pageX - parentOffset.left,
+              relY = e.pageY - parentOffset.top;
+              $(this).find('span').css({top:relY, left:relX})
+      })
+      .on('mouseout', function(e) {
+              var parentOffset = $(this).offset(),
+              relX = e.pageX - parentOffset.left,
+              relY = e.pageY - parentOffset.top;
+          $(this).find('span').css({top:relY, left:relX})
+      });
+    $('[href=#]').click(function(){return false});
+  });
